61375cf70c7a3ba63f4838a8f260e780790f3649
Mel/ <tchoikuemel06@gmail.com>
2024-08-29 01:12:30 +0100

    start app

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+		<div className='min-h-screen grid place-content-center'>
+			<h1 className='text-4xl font-semibold'>Hello, Nextjs❤️🧑‍💻</h1>
+		</div>
+	);
+}
+
+export default Home

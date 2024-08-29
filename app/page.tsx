cb6dd653effc2dd30eb594727d928ad51279acea
Mel/ <tchoikuemel06@gmail.com>
2024-08-29 05:56:49 +0100

    remove extend for tailwind.config and add hello nexjs

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from 'react';
 
 const Home = () => {
-  return (
-		<div className='min-h-screen grid place-content-center'>
+	return (
+		<div className='min-h-screen grid place-content-center  from-slate-200 via-emerald-100 to-red-100 bg-gradient-to-bl'>
 			<h1 className='text-4xl font-semibold'>Hello, Nextjs❤️🧑‍💻</h1>
+			<p className='text-base text-center mt-1 text-slate-500'>
+				<em>Happy coding</em>
+			</p>
 		</div>
 	);
-}
+};
 
-export default Home
+export default Home;
